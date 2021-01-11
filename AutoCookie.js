83520dcc00449fb3d99115fcad9906815a106ae3
Elekester <elekester@gmail.com>
2021-01-11 13:40:53 -0600

    Update AutoCookie.js

--- AutoCookie.js
+++ AutoCookie.js
@@ -302,7 +302,7 @@ AC.Helper.isEmpty = function(obj) {
  *  The functions called by Cookie Clicker to initialize the mod and save/load settings.
 *******************************************************************************/
 AC.Mod.init = function() {
-    var delay = seTimeout(function() {AC.Auto.load(AC.Config.Options.loaded); delay = cleaarTimeout(delay)}, 500);
+    var delay = setTimeout(function() {AC.Auto.load(AC.Config.Options.loaded); delay = cleaarTimeout(delay)}, 500);
     if (Game.prefs.popups) {
         Game.Popup("Auto Cookie " + AC.Version + " loaded.");
     } else {
